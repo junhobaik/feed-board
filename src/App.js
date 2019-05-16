e3657b3a7532bb85ce3f1fd3167ee18ca5adb711
Junho Baik <junhobaik@gmail.com>
2019-05-16 22:05:49 +0900

    RSS 불러오기 기능 구현
    - rss-parser 패키지를 이용
    - 간단하게 추가된 블로그와 블로그들의 포스트 리스트를 보여주도록 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import RSSParser from "rss-parser";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  state = {
+    links: ['https://d2.naver.com/d2.atom', 'https://meetup.toast.com/rss'],
+    result: []
+  };
 
-    </div>
-  );
+  _addLinks = url => {
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+    let parser = new RSSParser();
+    parser.parseURL(CORS_PROXY + url, (err, feed) => {
+      console.log(feed);
+
+      this.setState({
+        result: [...this.state.result, feed]
+      });
+
+      console.log(this.state);
+    });
+  };
+
+  componentDidMount() {
+    for (const link of this.state.links) {
+      this._addLinks(link);
+    }
+  }
+
+  render() {
+    const { result } = this.state;
+    let items = [];
+
+    const linkList = result.map((v, i) => {
+      items = [...items, ...v.items];
+
+      return (
+        <div className="link">
+          <a href={v.link} target="_blank" rel="noopener noreferrer">
+            {v.title}
+          </a>
+        </div>
+      );
+    });
+
+    const itemList = items
+      .sort((a, b) => {
+        return Date.parse(b.pubDate) - Date.parse(a.pubDate);
+      })
+      .map((v, i) => {
+        return (
+          <li>
+            <div>
+              <a href={v.link} target="_blank" rel="noopener noreferrer">
+                {v.title}
+              </a>
+            </div>
+          </li>
+        );
+      });
+
+    return (
+      <div id="App">
+        {linkList}
+        <ul>{itemList}</ul>
+      </div>
+    );
+  }
 }
-
-export default App;
