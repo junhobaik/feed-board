1dba97c7a1e4931756f56f4a992f9055c3735e61
Junho Baik <junhobaik@gmail.com>
2019-05-16 22:36:34 +0900

    eslint 및 prettier 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import RSSParser from "rss-parser";
-import "./App.css";
+import React, { Component } from 'react';
+import RSSParser from 'rss-parser';
+import './App.css';
 
 export default class App extends Component {
   state = {
     links: ['https://d2.naver.com/d2.atom', 'https://meetup.toast.com/rss'],
-    result: []
+    result: [],
   };
 
   _addLinks = url => {
-    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+    const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
     let parser = new RSSParser();
     parser.parseURL(CORS_PROXY + url, (err, feed) => {
       console.log(feed);
 
       this.setState({
-        result: [...this.state.result, feed]
+        result: [...this.state.result, feed],
       });
 
       console.log(this.state);
